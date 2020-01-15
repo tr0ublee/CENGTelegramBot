8348db09b97b8781a7a0670a8115cc0c7ed55b9f
tr0ublee <alperen.0311@gmail.com>
2020-01-15 18:09:42 +0300

    first commit

--- bot.py
+++ bot.py
@@ -1,6 +1,32 @@
+import telebot
+
+#init bot
 
 
-__main()__:
-    print("hello")
+#read token
+bot=telebot.TeleBot(input())
+
+
+
+#command = /smth
+#message = smth
+
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    global bot
+    bot.reply_to(message,"Agalar sa")
+
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    global bot
+    bot.reply_to(message,"su anlik yardim edilecek bir sey yok")
+
+@bot.message_handler(regexp="sa")
+def send_sa(message):
+    global bot
+    bot.reply_to(message,"as")
+
+bot.polling()
 
 
