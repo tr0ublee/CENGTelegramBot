69a7480ddfdfa3480e25a130c0e1e5eef9d0e589
tr0ublee <alperen.0311@gmail.com>
2020-01-15 19:02:14 +0300

    more info

--- bot.py
+++ bot.py
@@ -13,6 +13,18 @@ bot=telebot.TeleBot(input())
 #command = /smth
 #message = smth
 
+"""
+How to add your own function? 
+
+1-start a message_handler for your function
+
+2-while doing so, use the format regexp="\\byour_command\\b" (just like send_sa)
+
+3-to reach all the information about the message, for instance the whole message, 
+ you can reach it as message.text 
+"""
+
+
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
